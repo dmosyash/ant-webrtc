f4676699bf194b42f69c218b4c8d20f8027f3a9a
Yash Kochar <yashkochar@nexteducation.com>
2020-04-17 11:01:09 +0530

    alert added

--- webrtc_adaptor.js
+++ webrtc_adaptor.js
@@ -669,7 +669,7 @@ function WebRTCAdaptor(initialValues)
 
 		thiz.localStream.getTracks().forEach(track => track.stop());
 		thiz.mediaConstraints.video.facingMode = camera === 'front' ? 'user' : 'environemt';
-
+		alert(JSON.stringify(thiz.mediaConstraints));
 		navigator.mediaDevices.getUserMedia(thiz.mediaConstraints)
 			.then(function (stream) {
 				alert('ooo')
